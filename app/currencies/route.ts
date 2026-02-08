0ce26ba6f66e29f412f05a8d085a12a6fdfaecd5
Albin Kärvling <albin.karvling@atg.se>
2026-02-08 23:07:52 +0100

    fix old api requests

--- app/currencies/route.ts
+++ app/currencies/route.ts
@@ -1,18 +1,20 @@
 import { isValidCurrency } from "@/utils";
 
 export async function GET(request: Request) {
-    const res = await fetch(`${process.env.CURRENCY_API_URL}/latest?api_key=${process.env.CURRENCY_API_KEY}&base=USD`)
-    const data = await res.json();
-    
-    const rates = data.response.rates;
-    
-    for(const currency of Object.keys(rates)) {
-        if(!isValidCurrency(currency)) {
-            delete rates[currency];
-        }
-    }
+  const res = await fetch(
+    `${process.env.CURRENCY_API_URL}/latest?apikey=${process.env.CURRENCY_API_KEY}&base=USD`
+  );
+  const data = await res.json();
 
-    return Response.json(rates);
+  const rates = data.data;
+
+  for (const currency of Object.keys(rates)) {
+    if (!isValidCurrency(currency)) {
+      delete rates[currency];
+    }
+  }
+
+  return Response.json(rates);
 }
 
-export const runtime = 'edge';
+export const runtime = "edge";
